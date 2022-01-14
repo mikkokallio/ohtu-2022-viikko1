e8ef2093b3d5164d91c5870aa42d0aa21e0d8946
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-01-14 18:51:57 +0200

    Increase test coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,10 +13,23 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_negatiivinen_tilavuus_antaa_nollan(self):
+        varasto = Varasto(-0.1)
+        self.assertEqual(varasto.tilavuus, 0)
+
+    def test_negatiivinen_saldo_antaa_nollan(self):
+        varasto = Varasto(1, -1)
+        self.assertEqual(varasto.saldo, 0)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+    
+    def test_negaa_ei_voi_lisata(self):
+        self.varasto.lisaa_varastoon(10)
+        self.varasto.lisaa_varastoon(-100)
+        self.assertEqual(self.varasto.saldo, 10)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +51,20 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_lisays_ei_ylita_tilavuutta(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 0)
+    
+    def test_voi_ottaa_vain_mita_on(self):
+        self.varasto.ota_varastosta(100000000)
+        self.assertEqual(self.varasto.saldo, 0)
+    
+    def test_negaa_ei_voi_nyhjaista(self):
+        otettu = self.varasto.ota_varastosta(-10)
+        self.assertEqual(otettu, 0)
+        
+
+    def test_stringi_antaa_oikean(self):
+        stringi = str(self.varasto)
+        self.assertEqual(stringi, 'saldo = 0, vielä tilaa 10')
